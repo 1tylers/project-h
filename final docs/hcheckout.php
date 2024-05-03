09fcafe3b400f650ab40f43ff3559e85ea370987
1tylers <109832604+1tylers@users.noreply.github.com>
2024-05-03 02:28:37 -0700

    Update hcheckout.php

--- final docs/hcheckout.php
+++ final docs/hcheckout.php
@@ -90,7 +90,7 @@
     <div class="navbar">
         <a href="parts.php">Main Page</a>
         <a href="cart.php">Cart</a>
-        <a href="employee_login.php">Employee Login</a>
+        <a href="login.php">Employee Login</a>
     </div>
     <?php
     session_start();
@@ -284,7 +284,4 @@ if(isset($_POST['submit_order'])) {
     }
 }
 ?>
-
-
-
 
